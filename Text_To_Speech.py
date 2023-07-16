62a1a7002ec04334f6a9f8a37310d749eda9dbc0
maswud007 <99285655+maswud007@users.noreply.github.com>
2023-07-16 19:43:38 +0600

    Text_To_Speech.py

--- Text_To_Speech.py
+++ Text_To_Speech.py
@@ -1,1 +1,19 @@
+# importing libraries
+import os
+import pyttsx3
+
+print("Welcome to Text To Speech Converter")
+
+#  initializes the pyttsx3 engine
+reply = pyttsx3.init()
+while True:
+    user_input = input("Hey, type the sentence/sentences you want to pronounce: ")
+    if user_input == 'q':
+        break
+    # If you want to change the voice from male to female
+    #voice = reply.getProperty('voices')
+    #reply.setProperty('voice', voice[1].id)
+
+    reply.say(user_input)
+    reply.runAndWait()
 
